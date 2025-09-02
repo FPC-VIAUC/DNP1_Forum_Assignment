2afbc2602a107527ca48e9629be1d9b0d2166c55
FPC-VIAUC <354412@via.dk>
2025-09-02 10:36:06 +0200

    Entities

---
+++ Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace Entities;
+
+public class Comment{
+    private static int nextId = 0;
+
+    public int Id{ get; set; }
+    public string Body{ get; set; }
+    public int PostId{ get; set; }
+    public int UserId{ get; set; }
+
+    public Comment(string body, int postId, int userId){
+        Id = nextId++;
+        Body = body;
+        PostId = postId;
+        UserId = userId;
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Entities;
+
+public class User{
+    private static int nextId = 0;
+
+    public int Id{ get; set; }
+    public string Username{ get; set; }
+    public string Password{ get; set; }
+
+    public User(string username, string password){
+        Id = nextId++;
+        Username = username;
+        Password = password;
+    }
+}
