0e9b7ab55a9c4af34629a83eebb0bd0be081498b
FPC-VIAUC <354412@via.dk>
2025-09-30 09:40:54 +0200

    Add simple DTOs

---
+++ Shared/ApiContracts/CommentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiContracts;
+
+public class CommentDTO{
+    public int Id{ get; set; }
+    public string Body{ get; set; }
+    public int PostId{ get; set; }
+    public int UserId{ get; set; }
+}
