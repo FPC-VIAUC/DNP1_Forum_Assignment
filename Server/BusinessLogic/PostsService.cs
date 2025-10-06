aa920aa5a21f2d31ff58cf84f30a33ad75ba9a52
FPC-VIAUC <354412@via.dk>
2025-10-06 10:48:23 +0200

    Update posts service to auto align user id on update

--- Server/BusinessLogic/PostsService.cs
+++ Server/BusinessLogic/PostsService.cs
@@ -29,7 +29,8 @@ public class PostsService{
     public async Task UpdateAsync(Post post){
         Post currentPost = await postRepository.GetSingleAsync(post.Id);
         if (post.Title.Length == 0) throw new ArgumentException("The title of a post cannot be empty.");
-        if (currentPost.UserId != post.UserId) throw new ArgumentException("You cannot update the user who posted the post.");
+        //if (currentPost.UserId != post.UserId) throw new ArgumentException("You cannot update the user who posted the post.");
+        post.UserId = currentPost.UserId;
         await postRepository.UpdateAsync(post);
     }
 
