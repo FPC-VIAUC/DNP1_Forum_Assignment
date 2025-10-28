053cb96b5f393128ff6c68e854746f02c4aead1b
FPC-VIAUC <354412@via.dk>
2025-10-28 12:38:32 +0100

    Add auth controller to the web api

---
+++ Server/WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using ApiContracts;
+using BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase{
+    private UsersService usersService;
+
+    public AuthController(UsersService usersService){
+        this.usersService = usersService;
+    }
+
+    [HttpPost("login", Name = "login")]
+    public IActionResult Login([FromBody] LoginRequestDTO loginRequestDto){
+        try{
+            IQueryable<Entities.User> users = usersService.GetMany();
+            Entities.User? user = users.FirstOrDefault(u => u.Username == loginRequestDto.Username);
+            if (user == null || user.Password != loginRequestDto.Password) return Unauthorized();
+            return Ok(new UserDTO(){Id = user.Id, Username = user.Username});
+        } catch (Exception e){
+            return Unauthorized();
+        }
+    }
+}
---
+++ Shared/ApiContracts/LoginRequestDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace ApiContracts;
+
+public class LoginRequestDTO{
+    public string Username{ get; set; }
+    public string Password{ get; set; }
+}
