b73e0ca41ecb3766c6ea779e4534a7a63f30f626
FPC-VIAUC <354412@via.dk>
2025-11-18 11:27:28 +0100

    Add EFC Repositories and Context

---
+++ Server/EfcRepositories/ForumContext.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfcRepositories;
+
+public class ForumContext : DbContext{
+    public DbSet<User> Users{ get; set; }
+    public DbSet<Post> Posts{ get; set; }
+    public DbSet<Comment> Comments{ get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+        optionsBuilder.UseSqlite("Data Source = forum.db");
+    }
+}
